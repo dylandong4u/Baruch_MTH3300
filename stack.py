3f16bcb96c6368c255f9de711838f6a6d9ff73f0
dylandong4u <dylandong4u@gmail.com>
2022-10-22 00:11:19 -0400

    Updates

--- stack.py
+++ stack.py
@@ -7,24 +7,30 @@
     
 
 import turtle
-import math
 
 screen = turtle.Screen()
 block = turtle.Turtle()
+  
+def draw():
+
+  for i in range(4):
+    block.forward(10)
+    block.left(90)
 
 
 n = int(input("Enter an integer for n: "))
 
 for row in range(n):
-
-    for i in range(4):
+    for i in range(row+1):
+        draw()
         block.forward(10)
-        block.left(90)
+    
+    block.penup()
+    block.right(90)
+    block.forward(10)
+    block.right(90)
+    block.forward((row+1)*10)
+    block.right(180)
+    block.pendown()
     
 
-    block.right(90)
-    block.penup()
-    block.forward(10)
-    block.left(90)
-    block.forward(10)
-    block.pendown()
