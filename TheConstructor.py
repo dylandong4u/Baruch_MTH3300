dca0a01b73897a4ea9e60153d311f0e235e332e8
dylandong4u <dylandong4u@gmail.com>
2022-12-05 14:31:57 -0500

    HW10 Question

---
+++ TheConstructor.py
@@ -0,0 +1,42 @@
+class TTTBoard:
+
+	_board = [['', '', ''],
+			  ['', '', ''],
+			  ['', '', '']]
+
+	def __init__(self):
+		for idx_row in range(len(self._board)):
+			for idx_token in range(len(self._board[idx_row])):
+				if self._board[idx_row][idx_token] == '':
+					self._board[idx_row][idx_token] = ' '
+
+
+# I did this because when I initiate the constructor like this:
+
+'''
+class TTTBoard:
+	
+	def __init__(self):
+		self._board = [[' ', ' ', ' '],
+					   [' ', ' ', ' '],
+					   [' ', ' ', ' ']]
+'''
+# then I use win_for() method 
+
+'''
+def win_for(self, char):
+	win = False
+	
+	if TTTBoard().diag_full(char):
+		win = True
+	
+	else:
+		for row_col in range(3):
+			if TTTBoard().col_full(row_col, char):
+				win = True
+			elif TTTBoard().row_full(row_col, char):
+				win = True
+	
+	return win
+'''
+# self._board is always an empty 3x3 list
