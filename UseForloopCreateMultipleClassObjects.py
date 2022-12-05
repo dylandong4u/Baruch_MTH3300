6bc18e1a66dbd4884d4d383b00ce3bdadd4a0e48
dylandong4u <dylandong4u@gmail.com>
2022-12-05 14:32:10 -0500

    HW10 Question

---
+++ UseForloopCreateMultipleClassObjects.py
@@ -0,0 +1,11 @@
+new_list = []
+
+for i in range(10):
+	i = TTTBoard()
+	print(type(i)) # type(i) is class object
+	new_list.append(i)
+
+print(type(new_list[0])) # type of the list element is integer
+
+# Why? 
+
